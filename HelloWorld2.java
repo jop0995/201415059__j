73ce2ed516d53fa724b898709d3237882be83b92
201415059 _장민섭 <jms0995@hanmail.net>
2016-10-17 19:04:10 +0900

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,14 @@
+package com.sd.Hello;
+
+public class HelloWorld2 {
+  public void hello() {
+    System.out.println("Hello World from hello()");
+    }
+  public void hello(String name) {
+    System.out.printf("Hello World %s", name);
+    }
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    }
+    
+}
